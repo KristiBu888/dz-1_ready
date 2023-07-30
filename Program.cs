6b39949d1529a80c3665234f1cf71d8b2074cd45
KristiBu <106319724+KristiBu888@users.noreply.github.com>
2023-07-30 16:55:34 +0300

    dz-1_ready

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int max = 0;
+int min = 0;
+Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+   max = a;
+   min = b;
+}
+else
+{
+   max = b;
+   min = a;
+}
+
+Console.WriteLine("max = " + max + " и " + "min = " + min);
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int maxNumber = 0;
+Console.WriteLine("Введите число a");
+int aNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int bNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c");
+int cNumber = Convert.ToInt32(Console.ReadLine());
+
+if(aNumber > maxNumber)
+{
+    maxNumber = aNumber;
+}
+if(bNumber > maxNumber)
+{
+    maxNumber = bNumber;
+}
+if(cNumber > maxNumber)
+{
+    maxNumber = cNumber;
+}
+
+Console.WriteLine("max = " + maxNumber);
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
